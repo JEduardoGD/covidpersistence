32f7590002c99e78f72e5f98b5ee24659e732e02
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2024-01-14 16:08:31 -0600

    adding select between in covid table

--- src/main/java/egd/covid/persistence/repository/CovidEntityRepository.java
+++ src/main/java/egd/covid/persistence/repository/CovidEntityRepository.java
@@ -1,7 +1,10 @@
 package egd.covid.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import egd.covid.persistence.entity.CovidEntity;
@@ -10,4 +13,7 @@ import egd.covid.persistence.entity.CovidEntity;
 public interface CovidEntityRepository extends JpaRepository<CovidEntity, Long> {
 	@Query(value = "SELECT coalesce(max(id), 0) FROM CovidEntity")
 	public long getMaxId();
+	
+	@Query(value = "SELECT c FROM CovidEntity c WHERE c.id BETWEEN :from AND :to")
+	public List<CovidEntity> getBetween(@Param("from")long idFrom, @Param("to")long idTo);
 }
